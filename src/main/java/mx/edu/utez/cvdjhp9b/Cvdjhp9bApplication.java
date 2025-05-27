55b37776a0e4db66fbd9fa841a2d4729de0f781b
DiegoJesusP <spartfomi@outlook.com>
2025-05-27 15:50:32 -0600

    [FIRST COMMIT subida inicial de proyecto]

---
+++ src/main/java/mx/edu/utez/cvdjhp9b/Cvdjhp9bApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvdjhp9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvdjhp9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvdjhp9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cvdjhp9b/Cvdjhp9bApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvdjhp9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvdjhp9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
